aa9d1f72e653bc4e7416ff00906b80dca4cb00f4
hiwerx <hiwer@outlook.com>
2023-04-26 23:34:01 +0800

    gx

--- fuck_csdn.js
+++ fuck_csdn.js
@@ -1,17 +1,29 @@
 // ==UserScript==
 // @name         CSDN
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  去除登录弹窗，代码免登录复制
 // @author       You
 // @match        https://blog.csdn.net/*/article/details/*
 // @match        https://*.blog.csdn.net/article/details/*
+// @match        *://*.zhihu.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=csdn.net
+// @license      AGPL License
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+    let host = location.host
+    if(host.indexOf('zhihu')>-1){
+        cleanZhuhu()
+    }else if(host.indexOf('csdn')>-1){
+        cleanCsdn()
+    }
+    // Your code here...
+})();
+
+function cleanCsdn(){
     new Promise((resolve, reject) => {
         setTimeout(function() {
             // 关闭登录弹窗
@@ -35,8 +47,11 @@
         },1000);
     }).then((data) => {
         // 关闭常驻页面底部的 前往csdn按钮
-        let openAppTag =  document.querySelector("body > span")
-        if(openAppTag!=null)openAppTag.setAttribute('style','display:none;')
+        let openAppTags =  document.querySelectorAll(".feed-Sign-style-new")
+        for(let opTag of openAppTags){
+            opTag.setAttribute('class','')
+        }
+       // if(openAppTag!=null)openAppTag.setAttribute('style','display:none;')
         // 移出正文 打开app按钮
         $('.btn_open_app_prompt_div').remove()
         //移出广告
@@ -61,7 +76,7 @@
         if(codeStyle!=null&&codeStyle!=''){
             codeTag.setAttribute('style',codeStyle +'  user-select: all;')
         }else{
-             codeTag.setAttribute('style','  user-select: all;')
+            codeTag.setAttribute('style','  user-select: all;')
         }
     }
 
@@ -81,7 +96,11 @@
         tanchTag.click()
     }
 
-     // 继续监控页面
+    // 去除公众号广告
+    let extensionBox = document.querySelector('#blogExtensionBox')
+    if(extensionBox!=null)extensionBox.setAttribute('style','display:none;')
+
+    // 继续监控页面
     setInterval(function() {
         // 去除选浏览器继续还是打开app弹框,监控shadowbox，有冒头就按下去
         let pageContinueTags = document.querySelectorAll(".wap-shadowbox")
@@ -103,6 +122,23 @@
             link.removeAttribute('class')
         }
     },1000);
-    // Your code here...
-})();
+
+}
+
+function  cleanZhuhu(){
+    let interv = setInterval(function() {
+        console.log('监听弹窗')
+        let closeLogin = document.querySelector(".Button.Modal-closeButton.Button--plain")
+        if(closeLogin!=null)closeLogin.click()
+
+        let rightLogin = document.querySelector(".css-1ynzxqw")
+        if(rightLogin!=null)rightLogin.setAttribute('style','display:none;')
+    },500)
+
+    setTimeout(function() {
+        console.log('结束轮询')
+        clearInterval(interv)
+    },5000)
+
+}
 
